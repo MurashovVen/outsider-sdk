c717093b9040424e0e500b6b3171ab9c7057c423
murashovven <murashovven@gmail.com>
2023-08-07 15:36:02 +0600

    tmp

--- rabbitmq/channel.go
+++ rabbitmq/channel.go
@@ -16,6 +16,15 @@ type (
 	ChannelOption func(*Channel)
 )
 
+func MustConnect(url string, opts ...ChannelOption) *Channel {
+	ch, err := ChannelConnect(url, opts...)
+	if err != nil {
+		panic("can't connect rabbit: " + err.Error())
+	}
+
+	return ch
+}
+
 func ChannelConnect(url string, opts ...ChannelOption) (*Channel, error) {
 	channel := &Channel{
 		logger: logger.NewNop(),
